c6eda1c1b88a473903168a3a5594f411f5ecfeb3
Nrupatunga <nrupatunga.tunga@gmail.com>
2016-11-09 06:05:40 +0530

    Update char-rnn.py

--- char-rnn.py
+++ char-rnn.py
@@ -3,7 +3,8 @@
 #  Toy example of seq2seq modeling using vanilla RNN
 #  h(t) = tanh(Wxh*x(t) + Whh*h(t-1) + bxh + bhh)
 #  y(t) = Wyh*h(t) + byh
-#  x(t) is n dimensional embedding for one hot vector for vocabulary of length m
+#  x(t) is n dimensional embedding for one hot vector for vocabulary of length n
+
 import numpy as np
 import pdb
 
