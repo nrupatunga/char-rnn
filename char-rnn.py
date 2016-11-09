82c1da84ec673b4ed32248ac2584306ed39b1106
Nrupatunga <nrupatunga.tunga@gmail.com>
2016-11-09 15:09:06 +0530

    Update char-rnn.py

--- char-rnn.py
+++ char-rnn.py
@@ -6,7 +6,6 @@
 #  x(t) is n dimensional embedding for one hot vector for vocabulary of length n
 
 import numpy as np
-import pdb
 
 
 class VanillaRNN(object):
@@ -22,18 +21,19 @@ class VanillaRNN(object):
             num_hidden_units  - Number of hidden units
         '''
 
-        pdb.set_trace()
         data = []
         with open(txtFile, 'r') as f:
             data = f.read()
 
+        self.data = data
         self.data_len = len(data)
         chars = list(set(data))  # list of unique characters
         vocab_size = len(chars)
+        self.lr = lr
 
         #  Characters to index and vice versa
-        self.char_to_index = {i: c for i, c in enumerate(chars)}
-        self.index_to_char = {c: i for i, c in enumerate(chars)}
+        self.char_to_index = {c: i for i, c in enumerate(chars)}
+        self.index_to_char = {i: c for i, c in enumerate(chars)}
         self.vocab_size = vocab_size
         self.num_hidden_params = num_hidden_units
 
@@ -49,32 +49,71 @@ class VanillaRNN(object):
     def forward_backward(self, X, T, h_prev):
         '''forward pass'''
 
-        x_one_hot, h, y_pred, prob, loss = [], [], [], 0
-        h[-1] = h_prev
+        x_one_hot, h, y_pred, prob, loss = {}, {}, {}, {}, 0
+        h[-1] = np.copy(h_prev)
         for t in range(len(X)):
-            x_one_hot = np.zeros(self.vocab_size, 1)
+            x_one_hot[t] = np.zeros((self.vocab_size, 1))
             x_one_hot[t][X[t]] = 1
 
             #  RNN forward pass equations
-            h[t] = np.tanh(np.dot(self.wxh, x_one_hot) + np.dot(self.whh, h[t - 1]) + self.bh)
+            h[t] = np.tanh(np.dot(self.wxh, x_one_hot[t]) + np.dot(self.whh, h[t - 1]) + self.bh)
             y_pred[t] = np.dot(self.why, h[t]) + self.by
             prob[t] = np.exp(y_pred[t]) / np.sum(y_pred[t])
-            loss += -np.log(prob[t][T[t]])
+            loss += -np.log(prob[t][T[t], 0])
+
+        #  backward pass
+        dwxh, dwhh = np.zeros_like(self.wxh), np.zeros_like(self.whh)
+        dwhy, dbh, dby = np.zeros_like(self.why), np.zeros_like(self.bh), np.zeros_like(self.by)
+        dhnext = np.zeros_like(h[0])
+        for t in reversed(range(len(X))):
+            dy = np.copy(prob[t])
+            dy[T[t]] -= 1
+            dwhy = np.dot(h[t], dy.T)
+            dby += dby
+
+            dh = np.dot(self.why.T, dy) + dhnext
+            dhraw = (1 - h(t) * h[t]) * dh
+            dbh += dhraw
+            dwxh = np.dot(dhraw, x_one_hot[t].T)
+            dwhh = np.dot(dhraw, h[t - 1].T)
+            dhnext = np.dot(self.whh.T, dhraw)
+
+        for dparam in [dwxh, dwhh, dwhy, dbh, dby]:
+            np.clip(dparam, -5, 5, out=dparam)
+
+        return loss, dwxh, dwhh, dwhy, dbh, dby, h[len(X) - 1]
 
     def train(self):
         ''' Training the RNN'''
 
         ptr, sample_n = 0, 0
-        h_prev = np.zeros(self.num_hidden_params, 1)
+        h_prev = np.zeros((self.num_hidden_params, 1))
+        mwxh, mwhh, mwhy = np.zeros_like(self.wxh), np.zeros_like(self.whh), np.zeros_like(self.why)
+        mbh, mby = np.zeros_like(self.bh), np.zeros_like(self.by)  # memory variables for Adagrad
+        smooth_loss = -np.log(1.0 / self.vocab_size) * self.seq_len  # loss at iteration 0
 
         if ptr + self.seq_len >= self.data_len or sample_n is 0:
-            h_prev = np.zeros(self.num_hidden_params, 1)
+            h_prev = np.zeros((self.num_hidden_params, 1))
             ptr = 0
 
         inputs = [self.char_to_index[ch] for ch in self.data[ptr: ptr + self.seq_len]]
-        targets = [self.index_to_char[ch] for ch in self.data[ptr + 1: ptr + 1 + self.seq_len]]
-        self.forward_backward(inputs, targets, h_prev)
+        targets = [self.char_to_index[ch] for ch in self.data[ptr + 1: ptr + 1 + self.seq_len]]
+        loss, dwxh, dwhh, dwhy, dbh, dby, hprev = self.forward_backward(inputs, targets, h_prev)
+        smooth_loss = smooth_loss * 0.999 + loss * 0.001
+
+        if sample_n % 100 is 0:
+            print('Iteration {}, loss = {}'.format(sample_n, smooth_loss))
+
+        for param, dparam, mem in zip([self.wxh, self.whh, self.why, self.bh, self.by],
+                                      [dwxh, dwhh, dwhy, dbh, dby],
+                                      [mwxh, mwhh, mwhy, mbh, mby]):
+            mem = mem + dparam * dparam
+            param = param - (self.lr * dparam) / np.sqrt(mem + 1e-8)
+
+        ptr = ptr + self.seq_len
+        sample_n = sample_n + 1
 
 
 if __name__ == '__main__':
     objRNN = VanillaRNN('input.txt')
+    objRNN.train()
